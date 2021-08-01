0e00eec823bac0ba16f5c46b952047abea3fd4a1
sumi9518 <sumanyadav9518@gmail.com>
2021-08-01 13:44:59 +0200

    Added User Change password

--- client/src/App.js
+++ client/src/App.js
@@ -14,9 +14,14 @@ import { auth } from './firebase';
 import { useDispatch } from 'react-redux';
 import { identifier } from 'babel-types';
 import ForgotPassword from './pages/auth/ForgotPassword';
-import {currentUser} from './functions/auth';
-import History  from './pages/user/History';
+import { currentUser } from './functions/auth';
+import History from './pages/user/History';
 import UserRoutes from './components/nav/routes/UserRoutes';
+import Password from './pages/user/Password';
+import Wishlist from './pages/user/Wishlist';
+
+
+
 
 const App = () => {
 
@@ -28,20 +33,20 @@ const App = () => {
       if (user) {
         const idTokenResult = await user.getIdTokenResult();    //This data will persist in redx from DB (created endpoint)
         currentUser(idTokenResult.token)        //Below data dnot persist in redux after refresh, so we make req to own DB in App.js
-                .then((res) => {
-                    dispatch({
-                        type: "Logged_In_User",
-                        payload: {
-                            name: res.data.name,
-                            email: res.data.email,
-                            token: idTokenResult.token,
-                            role: res.data.role,
-                            uid: res.data._id
-                        },
-                    });
-    
-                })
-                .catch(err => console.log(err));
+          .then((res) => {
+            dispatch({
+              type: "Logged_In_User",
+              payload: {
+                name: res.data.name,
+                email: res.data.email,
+                token: idTokenResult.token,
+                role: res.data.role,
+                uid: res.data._id
+              },
+            });
+
+          })
+          .catch(err => console.log(err));
       }
     });
     //clean up
@@ -59,6 +64,8 @@ const App = () => {
         <Route exact path="/Register/Complete" component={RegisterComplete} />
         <Route exact path="/Forgot/Password" component={ForgotPassword} />
         <UserRoutes exact path="/user/History" component={History} />
+        <UserRoutes exact path="/user/Password" component={Password} />
+        <UserRoutes exact path="/user/Wishlist" component={Wishlist} />
       </Switch>
     </div>
   );
---
+++ client/src/pages/user/Password.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import UserNav from '../../components/nav/UserNav';
+import { auth } from '../../firebase';
+import { toast } from 'react-toastify';
+import LoadingToRedirect from '../../components/nav/routes/LoadingToRedirect';
+
+const Password = (e) => {
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        //console.log(password);
+        await auth.currentUser.updatePassword(password)
+            .then(() => {
+                setLoading(false);
+                setPassword("");
+                toast.success("Password Update Successfully");
+            })
+            .catch((err) => {
+                setLoading(false);
+                toast.error(err.message);
+            }
+            )
+    }
+
+    const UpdatePasswordForm = () => (
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label>Your Password</label>
+                <input
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="form-control"
+                    placeholder="Please enter your new password"
+                    disabled={loading}
+                    value={password}
+                />
+                <br />
+                <button className="btn btn-primary" disabled={!password || password.length < 6 || loading}>Submit</button>
+            </div>
+        </form>
+    )
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-md-2">
+                    <UserNav />
+                </div>
+                <div className="col">
+                    {loading ? (<h4 className="text-danger">Loading...</h4>) : (<h4>Password Update Form</h4>)}
+                    {UpdatePasswordForm()}
+                </div>
+            </div>
+        </div>
+    );
+
+};
+
+export default Password;
