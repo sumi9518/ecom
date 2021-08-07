b10b4d4d46ddc160e73178e2b9e8afa4f0dc22d4
sumi9518 <sumanyadav9518@gmail.com>
2021-08-07 14:11:54 +0200

    Added image preview

--- client/src/components/nav/forms/FileUpload.js
+++ client/src/components/nav/forms/FileUpload.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Resizer from 'react-image-file-resizer';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
+import {Avatar} from 'antd';
+
 
 const FileUpload = ({ values, setValues, setLoading }) => {
     const { user } = useSelector((state) => ({ ...state }));
@@ -59,6 +61,17 @@ const FileUpload = ({ values, setValues, setLoading }) => {
     }
     return (
 
+        <div>
+        <div>
+            {values.images && values.images.map((image)=>(
+                <Avatar 
+                key={image.public_id} 
+                src={image.url}
+                size={100}
+                className="m-3"
+                />
+            ))}
+        </div>
         <div className="row">
             <label className="btn btn-primary">
                 Choose files
@@ -70,6 +83,7 @@ const FileUpload = ({ values, setValues, setLoading }) => {
                     onChange={fileUploadAndResize}
                 />
             </label>
+        </div>
         </div>
     );
 };
