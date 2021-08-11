d82bc9e2d1f8aff0579c75b0ddd93311357d9f01
sumi9518 <sumanyadav9518@gmail.com>
2021-08-11 12:23:11 +0200

    Added Api get product total count

--- server/routes/product.js
+++ server/routes/product.js
@@ -8,15 +8,19 @@ const { authCheck, adminCheck } = require('../middlewares/auth');
 
 
 //controller
-const { create, listAll, remove, read, update, list } = require("../controllers/product"); //importing from  controller
+const { create, listAll, remove, read, update, list, productsCount } = require("../controllers/product"); //importing from  controller
 
 
 //route - take's the request eg /api and give response
+
+router.get('/products/abc', productsCount);
+
 router.post("/product", authCheck, adminCheck, create);
 router.get("/products/:count", listAll);
 router.delete("/product/:slug", authCheck, adminCheck, remove)
 router.get("/product/:slug", read);
 router.put("/product/:slug", authCheck, adminCheck, update);
 router.post('/products', list);
+
 
 module.exports = router;
