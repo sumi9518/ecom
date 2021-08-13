79100b2681cea85fc6f01c9d69ff67a419228a5d
sumi9518 <sumanyadav9518@gmail.com>
2021-08-13 17:58:51 +0200

    Added components to Single Product Page

--- client/src/components/cards/SingleProduct.js
+++ client/src/components/cards/SingleProduct.js
@@ -4,24 +4,29 @@ import { Link } from 'react-router-dom';
 import { HeartOutlined, ShoppingCartOutlined } from '@ant-design/icons';
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import laptop from '../../images/laptop.png';
+import ProductListItems from './ProductListItems';
 
-
-const { Meta } = Card;
 
 const SingleProduct = ({ product }) => {
-    const { title, description, images, slug } = product;
+    const { title, images } = product;
+
     return (
         <React.Fragment>
             <div className="col-md-7">
-                <Carousel showArrrows={true} autoPlay infiniteLoop>
-                    {images && images.map((i) =>
-                        <img src={i.url} key={i.public_id} />
+                {images && images.length ?
+                    <Carousel showArrrows={true} autoPlay infiniteLoop>
+                        {images && images.map((i) =>
+                            <img src={i.url} key={i.public_id} />
 
+                        )}
+                    </Carousel> : (
+                        <Card cover={<img src={laptop} className="mb-3 card-image" />}> </Card>
                     )}
-                </Carousel>
             </div>
 
             <div className="col-md-5">
+                <h1 className="bg-info p-3">{title}</h1>
                 <Card
                     actions={[
                         <React.Fragment>
@@ -33,8 +38,8 @@ const SingleProduct = ({ product }) => {
                             <HeartOutlined className="text-danger" /> <br />
                             Add to Wishlist</Link>
                     ]}>
-                    <Meta title={title} description={description} />
-                    <p>Price/category/subs</p>
+
+                    <ProductListItems product={product} />
                 </Card>
             </div>
         </React.Fragment>
--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -3,36 +3,28 @@ import { getProduct } from '../functions/Product';
 import SingleProduct from '../components/cards/SingleProduct';
 
 const Product = ({ match }) => {
+    const [product, setProduct] = useState([]);
 
-    const [product, setProduct] = useState({})
     const { slug } = match.params;
-
 
     useEffect(() => {
         loadSingleProduct();
     }, [slug]);
 
-    const loadSingleProduct = () => {
-        getProduct(slug)
-            .then((p) => {
-                setProduct(p.data);
-            })
-    }
+    const loadSingleProduct = () =>
+        getProduct(slug).then((res) => setProduct(res.data));
 
     return (
-
         <div className="container-fluid">
             <div className="row pt-4">
                 <SingleProduct product={product} />
             </div>
+
             <div className="row">
                 <div>Related products</div>
             </div>
-
         </div>
-
-    )
-
-}
+    );
+};
 
 export default Product;
