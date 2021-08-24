daf800d692deaf5e75c7a518deb698b993e009b5
sumi9518 <sumanyadav9518@gmail.com>
2021-08-24 16:04:16 +0200

    Added user validation against leaving rating

--- client/src/components/modal/RatingModal.js
+++ client/src/components/modal/RatingModal.js
@@ -3,15 +3,30 @@ import { toast } from 'react-toastify';
 import { Modal, Button } from 'antd';
 import { useSelector } from 'react-redux';
 import { StarOutlined } from '@ant-design/icons';
+import { useHistory } from 'react-router';
 
+//can't use history as this is not main comp (not called in app.js) but child of SingleProduct so using hook to get history
 
 const RatingModal = ({ children }) => {
 
     const { user } = useSelector((state) => ({ ...state }));
     const [modalVisible, setModalVisible] = useState(false);
+
+    let history = useHistory();
+
+    const handleModal = () => {
+        if (user && user.token) {
+            setModalVisible(true);
+
+        } else {
+            history.push("/login");
+        }
+    }
+
+
     return (
         <div>
-            <div onClick={() => setModalVisible(true)}>
+            <div onClick={handleModal}>
                 <StarOutlined className="text-warning" />
                 <br />
                 {" "}
