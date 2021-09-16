82778a0b793d7b3b7163ef51bca0da9163a10f45
sumi9518 <sumanyadav9518@gmail.com>
2021-09-16 11:34:18 +0200

    Added Category tab on Homepage

---
+++ client/src/components/category/CategoryList.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { getCategories } from '../../functions/category';
+
+const CategoryList = () => {
+    const [category, setCategory] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        getCategories()
+            .then(c => {
+                setCategory(c.data);
+                setLoading(false);
+            });
+    }, []);
+
+    const showCategory = () =>
+        category.map((c) => (
+            <div key={c._id} className="col btn btn-outline-primary btn-lg btn-block btn-raised m-3">
+                <Link to={`/category/${c.slug}`}>  {c.name}</Link>
+            </div>
+        ))
+
+
+    return (
+        <div className="container">
+            <div className="row">
+                {loading ? (<h4 className="text-center">Loading..</h4>) : showCategory()}
+            </div>
+
+        </div>
+    )
+};
+
+export default CategoryList;
