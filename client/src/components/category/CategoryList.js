633ec9129a553a3bee8048f44f208b110199b69c
sumi9518 <sumanyadav9518@gmail.com>
2021-09-19 19:51:52 +0200

    Added category page

--- client/src/components/category/CategoryList.js
+++ client/src/components/category/CategoryList.js
@@ -1,36 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { getCategory } from '../../functions/category';
+import { getCategories } from '../../functions/category';
 
 const CategoryList = () => {
-    const [category, setCategory] = useState([]);
+    const [categories, setCategories] = useState([]);
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         setLoading(true);
-        getCategory()
-            .then(c => {
-                setCategory(c.data);
-                setLoading(false);
-            });
+        getCategories().then((c) => {
+            setCategories(c.data);
+            setLoading(false);
+        });
     }, []);
 
-    const showCategory = () =>
-        category.map((c) => (
-            <div key={c._id} className="col btn btn-outline-primary btn-lg btn-block btn-raised m-3">
-                <Link to={`/category/${c.slug}`}>  {c.name}</Link>
+    const showCategories = () =>
+        categories.map((c) => (
+            <div
+                key={c._id}
+                className="col btn btn-outline-primary btn-lg btn-block btn-raised m-3"
+            >
+                <Link to={`/category/${c.slug}`}>{c.name}</Link>
             </div>
-        ))
-
+        ));
 
     return (
         <div className="container">
             <div className="row">
-                {loading ? (<h4 className="text-center">Loading..</h4>) : showCategory()}
+                {loading ? (
+                    <h4 className="text-center">Loading...</h4>
+                ) : (
+                    showCategories()
+                )}
             </div>
-
         </div>
-    )
+    );
 };
 
 export default CategoryList;
--- client/src/pages/category/CategoryHome.js
+++ client/src/pages/category/CategoryHome.js
@@ -3,6 +3,7 @@ import { getCategory } from '../../functions/category';
 import { Link } from 'react-router-dom';
 import ProductCard from '../../components/cards/ProductCard';
 
+
 const CategoryHome = ({ match }) => {
     const [category, setCategory] = useState({});
     const [products, setProducts] = useState([]);
@@ -16,13 +17,30 @@ const CategoryHome = ({ match }) => {
         getCategory(slug)
             .then(c => {
                 console.log(JSON.stringify(c.data, null, 4))
-                setCategory(c.data)
-            })
-    }, [])
+                setCategory(c.data.category);
+                setProducts(c.data.products);
+                setLoading(false);
+            });
+    }, []);
 
     return (
-        <div>
-            {match.params.slug}
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    {loading ? (
+                        <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">Loading...</h4>
+                    ) : (
+
+                        <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">{products.length} Products in "{category.name}" category</h4>)}
+                </div>
+            </div>
+            <div className="row">
+                {products.map((p) => (
+                    <div className="col-md-4" key={p._id}>
+                        <ProductCard product={p} />
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
